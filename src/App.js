3cb82025da7c6583b26b46d03e460108f6b99af2
KevinVeliz <mario-veliz258934@hotmail.com>
2022-09-06 04:01:44 -0500

    ProyectoWeb

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Login from './components/Login';
+import {Routes, Route} from "react-router-dom"
+import { Home } from './components/Home';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { app } from './firebase/firebase';
+import { useState } from 'react';
+import { Coordenadas } from './components/Coordenadas';
+import { Register } from './components/Register';
+
+const auth = getAuth(app)
+function App() {
+
+
+  const [usuario, setUsuario] = useState(null);
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUsuario(user)
+    }
+    else{
+      setUsuario(null)
+    }
+  });
+
+  return (
+
+      <Routes>
+        <Route path='/home' element={<Home/>}/> 
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import 'firebase/database'
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyBf7Y3G6mfUvZ31X1W1SvnKjhHhX8ugYkk",
+  authDomain: "ciclismoapp-60fa5.firebaseapp.com",
+  databaseURL: "https://ciclismoapp-60fa5-default-rtdb.firebaseio.com",
+  projectId: "ciclismoapp-60fa5",
+  storageBucket: "ciclismoapp-60fa5.appspot.com",
+  messagingSenderId: "272471059192",
+  appId: "1:272471059192:web:fa9df458e524a54ca4690d",
+  measurementId: "G-317N1Z4M8N"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+
+
